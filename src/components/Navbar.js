1ff4d991604e3e20613980a2bbbcf0eea3db0c7c
akadir8 <bickiabdulkadir12@gmail.com>
2023-10-23 18:34:37 +0300

    init repo

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom"; 
+// Import Link from react-router-dom
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav className="bg-slate-50 fixed z-10 mx-auto mr-auto w-screen max-w-[80vw]">
+        <ul>
+          <li>
+            <Link to="/">Home</Link> {/* Add a Link to the Home page */}
+          </li>
+          <li>
+            <Link to="/about">About</Link> {/* Add a Link to the About page */}
+          </li>
+          <li>
+            <Link to="/project">Project</Link> {/* Add a Link to the Contact page */}
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link> {/* Add a Link to the Contact page */}
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
